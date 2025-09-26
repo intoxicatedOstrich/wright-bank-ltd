9afb3a8977de63d5a1d64232ff3285b2aede4529
Augustinwright <48545626+Augustinwright@users.noreply.github.com>
2025-09-26 13:29:49 -0400

    Added transaction type as a parameter to the transation object. Added a net_amount class property accordingly using the @property decorator. Added logic such that the string format of the transaction displays a +,- before the transation for income, expense respectively

--- test_basic.py
+++ test_basic.py
@@ -5,8 +5,8 @@ from transaction import Transaction
 from datetime import datetime
 
 t1 = Transaction(50.00, "Groceries")
-t2 = Transaction(1500.00, "Salary")
-t3 = Transaction(-30.00, "Coffee Shop")
+t2 = Transaction(1500.00, "Salary", transaction_type = 'income')
+t3 = Transaction(30.00, "Coffee Shop")
 
 print("Testing Transaction Creation:")
 print()
